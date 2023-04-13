9ff7adcb31a725babf3bebebb065ef65aa958595
Endy Muhardin <endy.muhardin@gmail.com>
2023-04-13 09:39:24 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/spring/mobile/BelajarSpringMobileApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.spring.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringMobileApplication.class, args);
+	}
+
+}
